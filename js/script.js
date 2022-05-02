a5a7a62a7d062da46608fc130f1f17b745bfb539
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-05-02 15:21:30 +0200

    addNewFile function added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 {
+  let files = [];
   const filesInput = document.querySelector(".js-input");
   const filesElement = document.querySelector(".js-filesList");
 
+  const addNewFile = (newFile) => {
+    files = [...files, newFile];
+    console.log(files);
+  };
+
   const handleInputFiles = () => {
     const chosenFiles = [...filesInput.files];
-    console.log(chosenFiles);
-    console.log(chosenFiles.files);
+    console.log(filesInput.files);
+
+    for (const chosenFile of chosenFiles) {
+      addNewFile(chosenFile);
+    }
   };
 
   const init = () => {
