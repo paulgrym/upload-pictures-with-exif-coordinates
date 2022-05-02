6ac5f9ccc514c1dbfb028940ca4ca083ca800b8f
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-05-02 15:33:04 +0200

    renderFilesList and clearInput functions added

--- js/script.js
+++ js/script.js
@@ -1,23 +1,46 @@
 {
   let files = [];
   const filesInput = document.querySelector(".js-input");
-  const filesElement = document.querySelector(".js-filesList");
 
   const addNewFile = (newFile) => {
     files = [...files, newFile];
+    renderFilesList();
     console.log(files);
+  };
+
+  const renderFilesList = () => {
+    const filesList = files
+      .map(
+        (file, index) =>
+          `
+          <li class="list__item">
+            <span>${index + 1}.</span>
+            <span>${file.name}</span>
+          </li>
+        `
+      )
+      .join("");
+
+    const filesElement = document.querySelector(".js-filesList");
+    filesElement.innerHTML = filesList;
+  };
+
+  const clearInput = (filesInput) => {
+    filesInput.value = "";
   };
 
   const handleInputFiles = () => {
     const chosenFiles = [...filesInput.files];
-    console.log(filesInput.files);
 
     for (const chosenFile of chosenFiles) {
       addNewFile(chosenFile);
     }
+
+    clearInput(filesInput);
   };
 
   const init = () => {
+    renderFilesList();
     filesInput.addEventListener("change", handleInputFiles);
   };
 
